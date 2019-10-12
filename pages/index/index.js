a4425d180fe2813ec47d6aa6884902a018cd40b7
fionaaazhang <2391168535@qq.com>
2019-10-13 02:42:49 +0800

    toggleAll items remains to be fixed

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,69 @@
 Page({
+    
     data: {
+        input: "",
         todos: [
             {item: "weapp", completed:true}, 
             {item: "vue", completed:false}
-        ]
+        ],
+        leftCount: 1
+    },
+    
+    // 1.获取输入框的值,重新设置给页面
+    getInput: function(e){
+        
+        this.setData({input: e.detail.value})
+    },
+    //2.添加到todos
+    addToTodos: function(){
+        if(!this.data.input) return
+        var newTodos = this.data.todos
+        newTodos.push({
+            item: this.data.input,
+            completed: false
+        })
+        
+        this.setData({
+            todos: newTodos, 
+            input: "", 
+            leftCount: this.data.leftCount+1
+        })
+    },
+    toggleTodoHandler: function(e){
+        const index = e.currentTarget.dataset.index;
+
+        const item = this.data.todos[index]
+        item.completed = !item.completed 
+
+        const leftCount = this.data.leftCount+ (item.completed ? -1:1)
+
+        this.setData({todos: this.data.todos , leftCount: leftCount})
+        
+    },
+    delItem(e){
+        const index = e.currentTarget.dataset.index;
+        this.data.todos.splice(index,1)
+        this.setData({todos: this.data.todos})
+    },
+    toggleAllHandler(){
+        this.data.todos.forEach(item =>{
+            item.completed = !item.completed
+        })
+
+        this.setData({todos: this.data.todos})
+    },
+    clearDoneHandler(){
+        // const todos = []
+        // this.data.todos.forEach(item =>{
+        //     if (!item.completed) {
+        //         todos.push(item)
+        //     }  
+        // })
+
+        // 使用filter代替foreach
+        const todos = this.data.todos.filter(item => !item.completed)
+
+        this.setData({todos: todos})
     }
+
 })
